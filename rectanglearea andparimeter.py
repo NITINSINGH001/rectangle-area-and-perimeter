a0c72003cb6fafa7165704100c85cf97aa0fd328
NITINSINGH001 <143018967+NITINSINGH001@users.noreply.github.com>
2023-08-28 22:17:05 +0530

    Add files via upload

---
+++ rectanglearea andparimeter.py
@@ -0,0 +1,10 @@
+#input section
+length=int(input("enter the length"))
+breadth=int(input("enter the breadth"))
+#logic section
+peri=2*(length+breadth)
+#logic section
+area=length*breadth
+#display section
+print("the perimeter of rectangle is",peri)
+print("the area of rectangleis",area)
